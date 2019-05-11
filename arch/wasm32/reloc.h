a3c7c1cd9d75be4bca688944ee23aedccb8dbf13
Ian Douglas Scott <ian@iandouglasscott.com>
2019-05-11 12:12:24 -0700

    Add reloc.h that does nothing

---
+++ arch/wasm32/reloc.h
@@ -0,0 +1,18 @@
+// XXX
+
+#define LDSO_ARCH ""
+
+#define REL_SYMBOLIC    0
+#define REL_OFFSET32    0
+#define REL_GOT         0
+#define REL_PLT         0
+#define REL_RELATIVE    0
+#define REL_COPY        0
+#define REL_DTPMOD      0
+#define REL_DTPOFF      0
+#define REL_TPOFF       0
+#define REL_TLSDESC     0
+
+#define CRTJMP(pc,sp) ((void)0)
+
+#define GETFUNCSYM(fp, sym, got) ((void)0)
